e06667f5f9f3e95faffc80a7bdd519413e0d5c4b
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-01-18 20:50:25 +0200

    refactor: remove absolute div

--- src/App.tsx
+++ src/App.tsx
@@ -94,7 +94,6 @@ function App() {
         id='about'
         className='py-20 relative  bg-gradient-to-tr from-black via-gray-900 to-pink-900'
       >
-        <div className='absolute inset-0 pointer-events-none bg-gradient-to-tr from-black via-gray-900 to-pink- text-white'></div>
         <div className='container mx-auto px-6 text-white'>
           <div className='max-w-4xl mx-auto space-y-12'>
             <div className='space-y-6'>
